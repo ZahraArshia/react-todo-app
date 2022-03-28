e2153e1347b1c6a9105b813b1e64f84bd8382c3d
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-03-28 17:48:04 +0430

    edit function added

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,21 @@ import React from "react"
 import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
+  state = {
+    editing: false,
+  }
+
+  handleEditing = () => {
+  this.setState({
+    editing: true,
+  })
+}
+
+handleUpdatedDone = event => {
+  if (event.key === "Enter") {
+    this.setState({ editing: false })
+  }
+}
   render() {
     const completedStyle = {
       fontStyle: "italic",
@@ -10,15 +25,36 @@ class TodoItem extends React.Component {
       textDecoration: "line-through",
     }
 
+    let viewMode = {}
+    let editMode = {}
+
+    if (this.state.editing) {
+      viewMode.display = "none"
+    } else {
+      editMode.display = "none"
+    }
+
     return (
       <li className={styles.item}>
-        <input type="checkbox" className={styles.checkbox} checked={this.props.todo.completed} onChange={() => this.props.handleChangeProps(this.props.todo.id)}/>
-        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-          Delete
-        </button>
-        <span style={this.props.todo.completed ? completedStyle : null}>
-        {this.props.todo.title}
-        </span>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input type="checkbox" className={styles.checkbox} checked={this.props.todo.completed} onChange={() => this.props.handleChangeProps(this.props.todo.id)}/>
+          <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+            Delete
+          </button>
+          <span style={this.props.todo.completed ? completedStyle : null}>
+          {this.props.todo.title}
+          </span>
+        </div>
+        <input
+          type="text"
+          style={editMode}
+          className={styles.textInput}
+          value={this.props.todo.title}
+          onChange={e => {
+            this.props.setUpdate(e.target.value, this.props.todo.id)
+          }}
+          onKeyDown={this.handleUpdatedDone}
+        />
       </li>
     )
 }
